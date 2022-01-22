579a045a59c525b2e82922148d1b32e91efcc931
Andrew <neebet.2015@yandex.ru>
2022-01-22 14:00:26 +0200

    AWS DynamoDB CRUD
    database

--- amplify/backend/function/ProcessPayment/src/index.js
+++ amplify/backend/function/ProcessPayment/src/index.js
@@ -1,15 +1,40 @@
+const { CognitoIdentityServiceProvider } = require("aws-sdk");
+const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+const USER_POOL_ID = "us-east-2_DOFXEdRXN";
+const stripe = require("stripe")("sk_test_51KKglaIzoyncc1Q997Ow0EKGKdMGMJpptQGrJ2WMdyHKuLzycCbO7asHB6gtLXtNYH5smwOhsiRbpLZqYwVIjI5B003tGxGXai");
 
-
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+const getUserEmail = async (event) => {
+    const params = {
+        UserPoolId: USER_POOL_ID,
+        Username: event.identity.claims.username
     };
-    return response;
+    const user = await cognitoIdentityServiceProvider.adminGetUser(params).promise();
+    const { Value: email } = user.UserAttributes.find((attr) => {
+        if (attr.Name === "email") {
+            return attr.Value;
+        }
+    });
+    return email;
+};
+
+/*
+ * Get the total price of the order
+ * Charge the customer
+ */
+exports.handler = async (event) => {
+    try {
+        const { id, cart, total, address, token } = event.arguments.input;
+        const { username } = event.identity.claims;
+        const email = await getUserEmail(event);
+
+        await stripe.charges.create({
+            amount: total * 100,
+            currency: "usd",
+            source: token,
+            description: `Order ${new Date()} by ${email}`
+        });
+        return { id, cart, total, address, username, email };
+    } catch (err) {
+        throw new Error(err);
+    }
 };
---
+++ src/pages/Checkout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from "../components/CheckoutForm";
+
+const Checkout = () => {
+    const stripePromise = loadStripe('pk_test_51KKglaIzoyncc1Q9pvScriz4M97m4IekbCC9XHnfQKbc0ZkK7uVraYpjEro9l682Kqr5rMHdvnnOHX1I6BgzBBVW00eyXN6bZk');
+
+    return (
+        <section className="checkout-wrapper">
+            <AmplifyAuthenticator>
+                <Elements stripe={stripePromise}>
+                    <section>
+                        <h2>Time to Checkout?</h2>
+                        <CheckoutForm />
+                    </section>
+                </Elements>
+            </AmplifyAuthenticator>
+        </section>
+    )
+}
+
+export default Checkout
